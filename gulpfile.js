cb191cc7ec7b343c074f31e5e3e673d58d15f223
AfitovaViktoriya <afitovaviktoria@gmail.com>
2022-07-16 23:03:40 +0300

    feat: added components structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,12 @@ const {src, dest, parallel, series, watch} = require('gulp');
 const browserSync = require('browser-sync');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
-const del = require('del');
+const del = require('del');gulpfile.js
 const sass = require('gulp-sass')(require('sass'));
 const pug = require('gulp-pug');
+const FS = require('fs')
+
+const DATA = JSON.parse(FS.readFileSync('./data.json'))
 
 function browsersync() {
   browserSync.init({
@@ -15,30 +18,31 @@ function browsersync() {
 }
 
 function html() {
-    return src('src/index.pug')
-        .pipe(pug({
-            pretty: true,
-        }))
-        .pipe(dest('build'))
-        .pipe(browserSync.stream())
+  return src('src/index.pug')
+    .pipe(pug({
+      pretty: true,
+      locals: DATA
+    }))
+    .pipe(dest('build'))
+    .pipe(browserSync.stream())
 }
 
 function css() {
-    return src('src/styles.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            overrideBrowserslist: ['last 2 versions'],
-            grid: 'autoplace',
-        }))
-        .pipe(cleanCSS())
-        .pipe(dest('build'))
-        .pipe(browserSync.stream())
+  return src('src/styles.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 2 versions'],
+        grid: 'autoplace',
+      }))
+      .pipe(cleanCSS())
+      .pipe(dest('build'))
+      .pipe(browserSync.stream())
 }
 
 function images() {
-    return src('src/assets/images/**/*')
-        .pipe(dest('build/assets/images'))
-        .pipe(browserSync.stream())
+  return src('src/assets/images/**/*')
+    .pipe(dest('build/assets/images'))
+    .pipe(browserSync.stream())
 }
 
 function clear() {
